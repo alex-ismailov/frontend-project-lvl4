5551248ba6c0b65c7da6b13301d5f1c9d9b52d17
smlnx <alexander.is.dev@gmail.com>
2021-03-28 12:57:37 +0300

    Add(/) react to project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// @ts-check
+
+import React from 'react';
+
+import Chat from './components/Chat.jsx';
+
+const App = () => {
+  return <Chat />;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -7,28 +7,21 @@ import '../assets/application.scss';
 
 import gon from 'gon';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-const p = document.createElement('p');
-p.classList.add('card-text');
-p.textContent = 'It works!';
-
-const h5 = document.createElement('h5');
-h5.classList.add('card-title');
-h5.textContent = 'Project frontend l4 boilerplate';
-
-const cardBody = document.createElement('div');
-cardBody.classList.add('card-body');
-cardBody.append(h5, p);
-
-const card = document.createElement('div');
-card.classList.add('card', 'text-center');
-card.append(cardBody);
-
 const container = document.querySelector('#chat');
-container.append(card);
 
+ReactDOM.render(
+  <App />,
+  container,
+);
+
+// for dev mode, remove on prod
 console.log('it works!');
 console.log('gon', gon);
