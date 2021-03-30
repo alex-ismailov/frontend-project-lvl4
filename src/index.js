fd5680cb0fdcf9d6ef816b2eaaf5ecf69912dd07
smlnx <alexander.is.dev@gmail.com>
2021-03-30 21:13:30 +0300

    Refactor(src/index.js) move store configuration to src/init.jsx

--- src/index.js
+++ src/index.js
@@ -6,33 +6,13 @@ import '../assets/application.scss';
 // @ts-ignore
 import gon from 'gon';
 import ReactDOM from 'react-dom';
-import { configureStore } from '@reduxjs/toolkit';
-import channelsReducer from './slices/channels.js';
-import messagesReducer from './slices/messages.js';
-import currentChannelIdReducer from './slices/currentChannelId.js';
 import init from './init.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-// *****************************************
-// src/index.js В случае с реактом это не просто вызов функции, а полноценное монтирование.
-// Также все зависимости можно заводить здесь (rollbar, i18n, store) и прокидывать в аппу
-// init.jsx - задача этого файла вернуть кусок jsx который надо будет рендерить
-
-const preloadedState = { ...gon };
-const store = configureStore({
-  reducer: {
-    channels: channelsReducer,
-    messages: messagesReducer,
-    currentChannelId: currentChannelIdReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
-  preloadedState,
-});
-
+const vdom = init(gon);
 const container = document.querySelector('#chat');
 
-const vdom = init(store);
 ReactDOM.render(vdom, container);
--- src/init.jsx
+++ src/init.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import channelsReducer from './slices/channels.js';
+import messagesReducer from './slices/messages.js';
+import currentChannelIdReducer from './slices/currentChannelId.js';
 import App from './App.jsx';
 
-export default (store) => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+export default (gon) => {
+  const preloadedState = { ...gon };
+  const store = configureStore({
+    reducer: {
+      channels: channelsReducer,
+      messages: messagesReducer,
+      currentChannelId: currentChannelIdReducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+    preloadedState,
+  });
+
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
