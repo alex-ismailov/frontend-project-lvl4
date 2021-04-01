813c0338bb910af3c74096ba7dbb35b5ece2e122
smlnx <alexander.is.dev@gmail.com>
2021-04-01 13:32:11 +0300

    Add(__tests__/) frontend.test.js - but it doesn`t work. Fix at the review stage

---
+++ __tests__/frontend.test.js
@@ -0,0 +1,31 @@
+// @ts-check
+
+// import React from 'react';
+
+// import init from '../src/init.jsx';
+// import App from '../src/App.jsx';
+
+// import testingLibraryReact from '@testing-library/react';
+// const { render, screen } = testingLibraryReact;
+
+// import { render, screen } from '@testing-library/react';
+
+// const gon = {
+//   channels: [
+//     { id: 1, name: 'general', removable: false },
+//     { id: 2, name: 'random', removable: false },
+//   ],
+//   messages: [],
+//   currentChannelId: 1,
+// };
+
+// let App;
+// beforeEach(() => {
+//   App = init(gon);
+// });
+
+// test('renders App component', () => {
+//   render(<App />);
+
+//   screen.debug();
+// });
