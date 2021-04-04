d7a347cbef01a76a3c15cce823123c9edeb47245
smlnx <alexander.is.dev@gmail.com>
2021-04-04 16:31:35 +0300

    Refactor(src/components/Chat.jsx) use Messages and MessageForm components

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,40 +1,13 @@
 import React from 'react';
+import MessageForm from './MessageForm.jsx';
+import Messages from './Messages.jsx';
 
-const Chat = (props) => {
-  const { handleInputText, text } = props;
+const Chat = () => (
+  // const { handleInputText, text } = props;
 
-  return (
-    <div className="d-flex flex-column h-100">
-      {/* there will be rendered component messages-box */}
-      {/* <div id="messages-box" className="chat-messages overflow-auto mb-3">
-        <div className="text-break">
-          <b>Keith.Wolff43</b>: dfasdssdfa
-        </div>
-      </div> */}
-      <div className="mt-auto">
-        <form>
-          <div className="input-group">
-            <input
-              onChange={handleInputText}
-              name="body"
-              aria-label="body"
-              className="form-control"
-              value={text}
-            />
-            <div className="input-group-append">
-              <button
-                aria-label="submit"
-                type="submit"
-                className="btn btn-primary"
-              >
-                Submit
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-};
-
+  <div className="d-flex flex-column h-100">
+    <Messages />
+    <MessageForm />
+  </div>
+);
 export default Chat;
