f3c1e5a3cdd5a3f5c0d2914140aed3632f8fdee7
smlnx <alexander.is.dev@gmail.com>
2021-04-06 23:47:33 +0300

    Refactor(src/components/MessageForm.jsx) component, add sendMessage function

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,67 +1,100 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 import cn from 'classnames';
+import axios from 'axios';
+import UserNameContext from '../context/UserNameContext.js';
+import routes from '../routes.js';
 
-const MessageForm = () => (
-  <Formik
-    initialValues={{
-      body: '',
-    }}
-    validationSchema={yup.object().shape({
-      body: yup.string().required('Required'),
-    })}
-    validateOnBlur={false}
-    onSubmit={(values, actions) => {
-      alert(JSON.stringify(values, null, 2));
-      actions.setSubmitting(false);
-      actions.resetForm();
-      // здесь мы получим данные из формы values, т.е. сообщение
-      // далле передаем в async функцию которая отправит его на сервер
-      // это функция еще меняет состояние приложения
-      // loading: {
-      //   processState: loadingStateMap.idle, // см. 3 проект
-      //   error: null,
-      // },
-    }}
-  >
-    {({ isValid }) => {
-      const inputContainerClasses = cn('input-group', {
-        'has-validation': !isValid,
-      });
-      const inputClasses = cn('form-control', {
-        'is-invalid': !isValid,
-      });
+const sendMessage = async (currentChannelId, messageData) => {
+  const url = routes.channelMessagesPath(currentChannelId);
+  const postData = {
+    data: {
+      attributes: {
+        ...messageData,
+      },
+    },
+  };
+  try {
+    await axios.post(url, postData);
+    // вместо тела нового сообщения из response лучше воспользоваться
+    // websocketom
+  } catch (error) {
+    // если что пошло не так надо как то сообщить об этом
+    // через feedback под инпутом
+    console.log(error);
+  }
+};
 
-      return (
-        <div className="mt-auto">
-          <Form noValidate>
-            <div className={inputContainerClasses}>
-              <Field
-                type="text"
-                name="body"
-                aria-label="body"
-                className={inputClasses}
-              />
-              <div className="input-group-append">
-                <button
-                  type="submit"
-                  aria-label="submit"
-                  className="btn btn-primary"
-                >
-                  Submit
-                </button>
+const MessageForm = ({ currentChannelId }) => {
+  const userName = useContext(UserNameContext);
+
+  return (
+    <Formik
+      initialValues={{
+        body: '',
+      }}
+      validationSchema={yup.object().shape({
+        body: yup.string().required('Required'),
+      })}
+      validateOnBlur={false}
+      onSubmit={({ body }, actions) => {
+        // alert(JSON.stringify(values, null, 2));
+        actions.setSubmitting(false);
+        actions.resetForm();
+        // это функция еще меняет состояние приложения
+        // loading: {
+        //   processState: loadingStateMap.idle, // см. 3 проект
+        //   error: null,
+        // },
+        const messageData = {
+          nickname: userName,
+          body,
+        };
+        sendMessage(currentChannelId, messageData);
+      }}
+    >
+      {({ isValid }) => {
+        console.log(`user name from MessageForm component: ${userName}`);
+        const inputContainerClasses = cn('input-group', {
+          'has-validation': !isValid,
+        });
+        const inputClasses = cn('form-control', {
+          'is-invalid': !isValid,
+        });
+
+        return (
+          <div className="mt-auto">
+            <Form noValidate>
+              <div className={inputContainerClasses}>
+                <Field
+                  type="text"
+                  name="body"
+                  aria-label="body"
+                  className={inputClasses}
+                />
+                <div className="input-group-append">
+                  <button
+                    type="submit"
+                    aria-label="submit"
+                    className="btn btn-primary"
+                  >
+                    Submit
+                  </button>
+                </div>
+                <ErrorMessage
+                  name="body"
+                  render={(msg) => (
+                    <div className="invalid-feedback">{msg}</div>
+                  )}
+                />
               </div>
-              <ErrorMessage
-                name="body"
-                render={(msg) => <div className="invalid-feedback">{msg}</div>}
-              />
-            </div>
-          </Form>
-        </div>
-      );
-    }}
-  </Formik>
-);
+            </Form>
+          </div>
+        );
+      }}
+    </Formik>
+  );
+};
 
 export default MessageForm;
