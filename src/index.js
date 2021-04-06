6dd810e3166436b0e80ec52330b93ad6b1fb6b57
smlnx <alexander.is.dev@gmail.com>
2021-04-06 23:53:19 +0300

    Add(src/index.js) userName cookies settings

--- src/index.js
+++ src/index.js
@@ -6,13 +6,26 @@ import '../assets/application.scss';
 // @ts-ignore
 import gon from 'gon';
 import ReactDOM from 'react-dom';
+import { io } from 'socket.io-client';
+import Cookies from 'js-cookie';
+import faker from 'faker';
 import init from './init.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-const vdom = init(gon);
+if (!Cookies.get('userName')) {
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const randomNumber = faker.random.number(99);
+  const fakeName = `${firstName}.${lastName}${randomNumber}`;
+  Cookies.set('userName', fakeName, { expires: 30 });
+}
+
+const userName = Cookies.get('userName');
+const socket = io();
 const container = document.querySelector('#chat');
 
+const vdom = init(gon, socket, userName);
 ReactDOM.render(vdom, container);
