c2a19ddebbce6d632f3a44bcf1ef5695174d12b0
smlnx <alexander.is.dev@gmail.com>
2021-04-07 21:14:22 +0300

    Refactor(src/app/store.js) preloadedState shape, make it flatter

--- src/app/store.js
+++ src/app/store.js
@@ -3,20 +3,7 @@ import channelsReducer from '../features/channels/channelsSlice.js';
 import messagesReducer from '../features/messages/messagesSlice.js';
 import currentChannelIdReducer from '../features/channels/currentChannelIdSlice.js';
 
-const normalize = (data) =>
-  Object.entries(data).reduce(
-    (acc, [key, value]) => ({
-      ...acc,
-      [key]: {
-        [key]: value,
-      },
-    }),
-    {}
-  );
-
-export default (initialData = null) => {
-  const preloadedState = initialData ? normalize(initialData) : initialData;
-
+export default (preloadedState = null) => {
   const store = configureStore({
     reducer: {
       channels: channelsReducer,
