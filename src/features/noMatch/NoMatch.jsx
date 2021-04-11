e5b66feb7123ba0bd25b00bfe457f25a0e333251
smlnx <alexander.is.dev@gmail.com>
2021-04-11 13:54:56 +0300

    Add(src/features/noMatch/) NoMatch.jsx - page 404

---
+++ src/features/noMatch/NoMatch.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const NoMatch = () => {
+  const location = useLocation();
+
+  return (
+    <div>
+      <h1>404</h1>
+      <h3>
+        Not found <code>{location.pathname}</code>
+      </h3>
+      <p>Maybe this page moved? Got deleted?</p>
+      <hr />
+      <p>
+        Let`s go <a href="/">HOME</a> and try from there.
+      </p>
+    </div>
+  );
+};
+
+export default NoMatch;
