9c38bf5b62c196e536747137aace953ae5ddbfdd
smlnx <alexander.is.dev@gmail.com>
2021-04-12 07:59:24 +0300

    Add(src/common/routes.js) loginPath

--- src/common/routes.js
+++ src/common/routes.js
@@ -9,4 +9,5 @@ export default {
   channelPath: (id) => [host, prefix, 'channels', id].join('/'),
   channelMessagesPath: (id) =>
     [host, prefix, 'channels', id, 'messages'].join('/'),
+  loginPath: () => [host, prefix, 'login'].join('/'),
 };
