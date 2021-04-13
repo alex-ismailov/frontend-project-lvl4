4a46838a102eea2d68916dd72c1103608c0a363b
smlnx <alexander.is.dev@gmail.com>
2021-04-13 23:41:39 +0300

    Refactor(src/features/chat/Chat.jsx) render Chat with Header component

--- src/features/chat/Chat.jsx
+++ src/features/chat/Chat.jsx
@@ -1,38 +1,46 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import _ from 'lodash';
+import { useHistory } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import Channels from './channels/Channels.jsx';
 import Messages from './messages/Messages.jsx';
 import MessageForm from './messagesForm/MessageForm.jsx';
+import Header from '../../common/Header.jsx';
 
-// const Chat = () => (
-//   <Row className="flex-grow-1 h-75 pb-3">
-//     <Col xs={3} className="border-right">
-//       <Channels />
-//     </Col>
-//     <Col className="h-100">
-//       <div className="d-flex flex-column h-100">
-//         <Messages />
-//         <MessageForm />
-//       </div>
-//     </Col>
-//   </Row>
-// );
+const ExitButton = () => {
+  const { t } = useTranslation();
+  const history = useHistory();
+
+  const handleClick = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  return (
+    <Button onClick={handleClick} variant="primary">
+      {t('exit')}
+    </Button>
+  );
+};
 
 const Chat = () => {
   console.log(`rendering of <Chat/>: ${_.uniqueId()}`);
   return (
-    <Row className="flex-grow-1 h-75 pb-3">
-      <Col xs={3} className="border-right">
-        <Channels />
-      </Col>
-      <Col className="h-100">
-        <div className="d-flex flex-column h-100">
-          <Messages />
-          <MessageForm />
-        </div>
-      </Col>
-    </Row>
+    <>
+      <Header render={() => <ExitButton />} />
+      <Row className="flex-grow-1 h-75 pb-3">
+        <Col xs={3} className="border-right">
+          <Channels />
+        </Col>
+        <Col className="h-100">
+          <div className="d-flex flex-column h-100">
+            <Messages />
+            <MessageForm />
+          </div>
+        </Col>
+      </Row>
+    </>
   );
 };
 
