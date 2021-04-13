a3cd375375f6b8aa26ded5d114b0d979611f3d9d
smlnx <alexander.is.dev@gmail.com>
2021-04-13 22:45:34 +0300

    Refactor(src/index.js) remove setting username into Coockie. Remove gon import

--- src/index.js
+++ src/index.js
@@ -6,27 +6,14 @@ import '../assets/application.scss';
 // @ts-ignore
 // import gon from 'gon';
 import ReactDOM from 'react-dom';
-import Cookies from 'js-cookie';
-import faker from 'faker';
 import init from './app/init.jsx';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-if (!Cookies.get('userName')) {
-  const firstName = faker.name.firstName();
-  const lastName = faker.name.lastName();
-  const randomNumber = faker.random.number(99);
-  const fakeName = `${firstName}.${lastName}${randomNumber}`;
-  Cookies.set('userName', fakeName, { expires: 30 });
-}
-
-const gon = {};
-
-const userName = Cookies.get('userName');
 const container = document.querySelector('#chat');
 
-const vdom = init(gon, userName);
+const vdom = init();
 
 ReactDOM.render(vdom, container);
