ac373ffb651bad9d18a62f54420ac70fb05bd2e3
smlnx <alexander.is.dev@gmail.com>
2021-04-13 22:41:16 +0300

    Refactor(src/app/App.jsx) take username from localStorage.username and put it to UserNameProvider

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,6 +5,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Chat from '../features/chat/Chat.jsx';
 import LoginForm from '../features/loginForm/LoginForm.jsx';
 import NoMatch from '../features/noMatch/NoMatch.jsx';
+import { UserNameProvider } from '../context/UserNameContext.js';
 
 const App = () => (
   <div className="d-flex flex-column h-100">
@@ -13,15 +14,24 @@ const App = () => (
         Hexlet Chat
       </a>
       {/* После авторизации здесь появляется кнопка выйти */}
+      {/* { localStorage.token && <button type="button" className="btn btn-primary">Выйти</button>} */}
     </nav>
     <Switch>
       <Route path="/login" render={() => <LoginForm />} />
       <Route
         exact
         path="/"
-        render={() =>
-          localStorage.token ? <Chat /> : <Redirect push to="/login" />
-        }
+        render={() => {
+          if (localStorage.token) {
+            const { username } = localStorage;
+            return (
+              <UserNameProvider value={username}>
+                <Chat />
+              </UserNameProvider>
+            );
+          }
+          return <Redirect push to="/login" />;
+        }}
       />
       <Route path="*" render={() => <NoMatch />} />
     </Switch>
