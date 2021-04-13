a40b580ec9211dbbed2a0c2eaaf8cb49e6e39f98
smlnx <alexander.is.dev@gmail.com>
2021-04-13 23:41:15 +0300

    Refactor(src/features/loginForm/LoginForm.jsx) render LoginForm with Header component

--- src/features/loginForm/LoginForm.jsx
+++ src/features/loginForm/LoginForm.jsx
@@ -8,11 +8,7 @@ import cn from 'classnames';
 import axios from 'axios';
 import Feedback from '../../common/Feedback.jsx';
 import routes from '../../common/routes.js';
-
-// это потом уйдет в регистрацию
-// const authorizationScheme = yup.object().shape({
-//   nickName: yup.string(),
-// });
+import Header from '../../common/Header.jsx';
 
 const login = async (loginData, actions, history) => {
   actions.setSubmitting(false);
@@ -34,68 +30,71 @@ const LoginForm = () => {
   const history = useHistory();
 
   return (
-    <div className="container-fluid">
-      <Row className="justify-content-center pt-5">
-        <Col sm="4">
-          <Formik
-            initialValues={{
-              username: '',
-              password: '',
-            }}
-            onSubmit={(loginData, actions) =>
-              login(loginData, actions, history)
-            }
-            validateOnBlur={false}
-            validateOnChange={false}
-          >
-            {({ isValid, errors: errorKey }) => {
-              const inputClasses = cn('form-control', {
-                'is-invalid': !isValid,
-              });
-              return (
-                <Form className="p-3">
-                  <FormGroup>
-                    <FormLabel htmlFor="username">
-                      {t('yourNickname')}
-                    </FormLabel>
-                    <Field
-                      autoFocus
-                      type="text"
-                      name="username"
-                      required
-                      id="username"
-                      className={inputClasses}
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <FormLabel htmlFor="password">{t('password')}</FormLabel>
-                    <Field
-                      type="password"
-                      name="password"
-                      required
-                      id="password"
-                      className={inputClasses}
-                    />
-                    {!isValid && <Feedback message={t(errorKey)} />}
-                  </FormGroup>
-                  <Button
-                    type="submit"
-                    variant="outline-primary"
-                    className="w-100 mb-3"
-                  >
-                    {t('login')}
-                  </Button>
-                  <div className="d-flex flex-column align-items-center">
-                    <span className="small mb-2">{t('haveNoAccount')}</span>
-                    <a href="/signup">{t('registration')}</a>
-                  </div>
-                </Form>
-              );
-            }}
-          </Formik>
-        </Col>
-      </Row>
-    </div>
+    <>
+      <Header render={null} />
+      <div className="container-fluid">
+        <Row className="justify-content-center pt-5">
+          <Col sm="4">
+            <Formik
+              initialValues={{
+                username: '',
+                password: '',
+              }}
+              onSubmit={(loginData, actions) =>
+                login(loginData, actions, history)
+              }
+              validateOnBlur={false}
+              validateOnChange={false}
+            >
+              {({ isValid, errors: errorKey }) => {
+                const inputClasses = cn('form-control', {
+                  'is-invalid': !isValid,
+                });
+                return (
+                  <Form className="p-3">
+                    <FormGroup>
+                      <FormLabel htmlFor="username">
+                        {t('yourNickname')}
+                      </FormLabel>
+                      <Field
+                        autoFocus
+                        type="text"
+                        name="username"
+                        required
+                        id="username"
+                        className={inputClasses}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <FormLabel htmlFor="password">{t('password')}</FormLabel>
+                      <Field
+                        type="password"
+                        name="password"
+                        required
+                        id="password"
+                        className={inputClasses}
+                      />
+                      {!isValid && <Feedback message={t(errorKey)} />}
+                    </FormGroup>
+                    <Button
+                      type="submit"
+                      variant="outline-primary"
+                      className="w-100 mb-3"
+                    >
+                      {t('login')}
+                    </Button>
+                    <div className="d-flex flex-column align-items-center">
+                      <span className="small mb-2">{t('haveNoAccount')}</span>
+                      <a href="/signup">{t('registration')}</a>
+                    </div>
+                  </Form>
+                );
+              }}
+            </Formik>
+          </Col>
+        </Row>
+      </div>
+    </>
   );
 };
 
