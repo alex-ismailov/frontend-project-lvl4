8bdd9365db3633ff22166c484d30ca645dacd745
smlnx <alexander.is.dev@gmail.com>
2021-04-13 23:36:08 +0300

    Refactor(src/app/App.jsx) move header nav to own component Header.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -9,13 +9,6 @@ import { UserNameProvider } from '../context/UserNameContext.js';
 
 const App = () => (
   <div className="d-flex flex-column h-100">
-    <nav className="mb-3 navbar navbar-expand-lg navbar-light bg-light">
-      <a className="mr-auto navbar-brand" href="/">
-        Hexlet Chat
-      </a>
-      {/* После авторизации здесь появляется кнопка выйти */}
-      {/* { localStorage.token && <button type="button" className="btn btn-primary">Выйти</button>} */}
-    </nav>
     <Switch>
       <Route path="/login" render={() => <LoginForm />} />
       <Route
