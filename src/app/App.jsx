c54a47081d85801f188243a8e277ea932365d78d
smlnx <alexander.is.dev@gmail.com>
2021-04-13 09:39:56 +0300

    Bug. history push to / in LoginForm component doesn`t work

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,33 +1,70 @@
 // @ts-check
 
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import Chat from '../features/chat/Chat.jsx';
 import LoginForm from '../features/loginForm/LoginForm.jsx';
 import NoMatch from '../features/noMatch/NoMatch.jsx';
 
-const App = () => (
-  <div className="d-flex flex-column h-100">
-    <nav className="mb-3 navbar navbar-expand-lg navbar-light bg-light">
-      <a className="mr-auto navbar-brand" href="/">
-        Hexlet Chat
-      </a>
-      {/* После авторизации здесь появляется кнопка выйти */}
-    </nav>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Chat />
-        </Route>
-        <Route path="/login">
-          <LoginForm />
-        </Route>
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
-    </Router>
-  </div>
-);
+// const App = () => (
+//   <div className="d-flex flex-column h-100">
+//     <nav className="mb-3 navbar navbar-expand-lg navbar-light bg-light">
+//       <a className="mr-auto navbar-brand" href="/">
+//         Hexlet Chat
+//       </a>
+//       {/* После авторизации здесь появляется кнопка выйти */}
+//     </nav>
+//     <Router>
+//       <Switch>
+//         <Route exact path="/">
+//           {/* <Chat /> */}
+//           { localStorage.token ? <Chat /> : <Redirect push to="/login" /> }
+//         </Route>
+//         <Route path="/login">
+//           <LoginForm />
+//           {/* { localStorage.token ? <Chat /> : <Redirect to="/login" /> } */}
+//         </Route>
+//         <Route path="*">
+//           <NoMatch />
+//         </Route>
+//       </Switch>
+//     </Router>
+//   </div>
+// );
+
+const App = () => {
+  console.log(localStorage);
+
+  return (
+    <div className="d-flex flex-column h-100">
+      <nav className="mb-3 navbar navbar-expand-lg navbar-light bg-light">
+        <a className="mr-auto navbar-brand" href="/">
+          Hexlet Chat
+        </a>
+        {/* После авторизации здесь появляется кнопка выйти */}
+      </nav>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {/* <Chat /> */}
+            {localStorage.token ? <Chat /> : <Redirect push to="/login" />}
+          </Route>
+          <Route path="/login">
+            <LoginForm />
+            {/* { localStorage.token ? <Chat /> : <Redirect to="/login" /> } */}
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
--- src/features/loginForm/LoginForm.jsx
+++ src/features/loginForm/LoginForm.jsx
@@ -6,6 +6,7 @@ import { Formik, Form, Field } from 'formik';
 import { Row, Col, Button, FormGroup, FormLabel } from 'react-bootstrap';
 import cn from 'classnames';
 import axios from 'axios';
+import _ from 'lodash';
 import Feedback from '../../common/Feedback.jsx';
 import routes from '../../common/routes.js';
 
@@ -22,7 +23,10 @@ const login = async (loginData, actions, history) => {
     localStorage.setItem('token', token);
     // надо сохранить username в контекст
     history.push('/');
+    // history.location('/');
   } catch (error) {
+    console.log(error);
+    console.log('CATCH BLOCK');
     actions.setErrors('invalidUsernameOrPassword'); // good
     // надо еще что-то сделать на случай Network error
   }
@@ -31,6 +35,25 @@ const login = async (loginData, actions, history) => {
 const LoginForm = () => {
   const { t } = useTranslation();
   const history = useHistory();
+
+  // if (localStorage.token) {
+  //   return (
+  //     <Redirect to="/" />
+  //   );
+  // }
+
+  console.log(`rendering of LoginForm: ${_.uniqueId()}`);
+  // console.log(history.length);
+  // console.log(`history.location: ${JSON.stringify(history.location, null, '  ')}`);
+  // console.log(history.action);
+  // console.log(`The current URL is ${location.pathname}`);
+
+  // history.listen((location, action) => {
+  //   console.log(
+  //     `The current URL is ${location.pathname}${location.search}${location.hash}`
+  //   );
+  //   console.log(`The last navigation action was ${action}`);
+  // });
 
   return (
     <div className="container-fluid">
