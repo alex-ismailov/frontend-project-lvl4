2689f4ca322134d91dcefdb508421f7c3a7f4da7
smlnx <alexander.is.dev@gmail.com>
2021-04-15 12:03:22 +0300

    Refactor(src/app/store.js) add default value to preloadedState parameter

--- src/app/store.js
+++ src/app/store.js
@@ -3,7 +3,7 @@ import channelsReducer from '../features/chat/channels/channelsSlice.js';
 import messagesReducer from '../features/chat/messages/messagesSlice.js';
 import currentChannelIdReducer from '../features/chat/channels/currentChannelIdSlice.js';
 
-export default (preloadedState = null) => {
+export default (preloadedState = {}) => {
   const store = configureStore({
     reducer: {
       channels: channelsReducer,
