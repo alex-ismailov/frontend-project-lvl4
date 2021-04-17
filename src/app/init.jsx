49f43857be6c5f618b5d4d049b5f079de0b13548
smlnx <alexander.is.dev@gmail.com>
2021-04-17 07:56:33 +0300

    Add(src/app/init.jsx) yup setLocale

--- src/app/init.jsx
+++ src/app/init.jsx
@@ -3,15 +3,19 @@ import '../i18n.js';
 import { Provider } from 'react-redux';
 import { io } from 'socket.io-client';
 import { BrowserRouter } from 'react-router-dom';
+import { setLocale } from 'yup';
 import createStore from './store.js';
 import { addNewMessage } from '../features/chat/messages/messagesSlice.js';
 import App from './App.jsx';
 import { SocketProvider } from '../context/SocketContext.js';
 import { addChannel } from '../features/chat/channels/channelsSlice.js';
 import { setCurrentChannelId } from '../features/chat/channels/currentChannelIdSlice.js';
+import yupDictionary from '../locales/yupDictionary.js';
 
 export default (preloadedState = {}) => {
   const store = createStore(preloadedState);
+
+  setLocale(yupDictionary);
 
   const socket = io();
   socket.on('newMessage', (message) => {
