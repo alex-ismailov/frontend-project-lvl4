185ac4ea29f3d5e9b49ed59a4d7ef9f29a89f75a
smlnx <alexander.is.dev@gmail.com>
2021-04-17 08:14:22 +0300

    Add(src/app/store.js) loadingReducer

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import channelsReducer from '../features/chat/channels/channelsSlice.js';
 import messagesReducer from '../features/chat/messages/messagesSlice.js';
 import currentChannelIdReducer from '../features/chat/channels/currentChannelIdSlice.js';
 import modalReducer from '../features/modal/ModalWindowSlice.js';
+import loadingReducer from './loadingSlice.js';
 
 export default (preloadedState = {}) => {
   const store = configureStore({
@@ -11,6 +12,7 @@ export default (preloadedState = {}) => {
       messages: messagesReducer,
       currentChannelId: currentChannelIdReducer,
       modal: modalReducer,
+      loading: loadingReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
     preloadedState,
