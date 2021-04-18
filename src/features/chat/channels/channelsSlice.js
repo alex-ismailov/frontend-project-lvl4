7dc7429152a16458bff9911e5f82f710603c7695
smlnx <alexander.is.dev@gmail.com>
2021-04-18 17:52:02 +0300

    Add(src/features/chat/channels/channelsSlice.js) removeChannel reducer

--- src/features/chat/channels/channelsSlice.js
+++ src/features/chat/channels/channelsSlice.js
@@ -6,13 +6,15 @@ const slice = createSlice({
   name: 'channels',
   initialState: [],
   reducers: {
-    addChannel: (state, { payload: { channel } }) => { // eslint-disable-line
+    addChannel: (state, { payload: { channel } }) => {
       state.push(channel);
     },
+    removeChannel: (state, { payload: { id } }) =>
+      state.filter((channel) => channel.id !== id),
     initChannels: (state, { payload: { channels } }) => channels,
   },
 });
 
-export const { addChannel, initChannels } = slice.actions;
+export const { addChannel, removeChannel, initChannels } = slice.actions;
 
 export default slice.reducer;
