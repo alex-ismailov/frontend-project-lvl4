866e47e33a48d0c9b0d82c3a766a0428ab104460
smlnx <alexander.is.dev@gmail.com>
2021-04-18 17:54:48 +0300

    Add(src/features/chat/messages/messagesSlice.js) extraReducers on remove channel action for appropriate messages removing

--- src/features/chat/messages/messagesSlice.js
+++ src/features/chat/messages/messagesSlice.js
@@ -1,16 +1,22 @@
 // @ts-check
 
 import { createSlice } from '@reduxjs/toolkit';
+import { removeChannel } from '../channels/channelsSlice.js';
 
 const slice = createSlice({
   name: 'messages',
   initialState: [],
   reducers: {
     addNewMessage: (state, { payload: { message } }) => {
-      // @ts-ignore
       state.push(message);
     },
     initMessages: (state, { payload: { messages } }) => messages,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(removeChannel, (state, action) => {
+      const { id } = action.payload;
+      return state.filter((message) => message.channelId !== id);
+    });
   },
 });
 
