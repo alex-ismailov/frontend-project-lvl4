75056747e8bc266378589004f655a985d5e3b81a
smlnx <alexander.is.dev@gmail.com>
2021-04-19 07:43:27 +0300

    Refactor(src/features/modal/ModalWindow.jsx) name actions in verb form in submitActionsMap. Fix removeChannel in RemovingPanel

--- src/features/modal/ModalWindow.jsx
+++ src/features/modal/ModalWindow.jsx
@@ -5,27 +5,32 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { FormGroup, Modal, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import * as yup from 'yup';
-import { toggleModal, buildModalConfig } from './ModalWindowSlice.js';
+import {
+  toggleModal,
+  buildModalConfig,
+  modalTypesMap,
+} from './ModalWindowSlice.js';
 import Feedback from '../../common/Feedback.jsx';
 import SocketContext from '../../context/SocketContext.js';
 import { loadingStatesMap, setLoadingState } from '../../app/loadingSlice.js';
 
 const submitActionsMap = {
-  adding: 'newChannel',
-  removing: 'removeChannel',
-  renaming: 'renameChannel',
+  add: 'newChannel',
+  remove: 'removeChannel',
+  rename: 'renameChannel',
 };
 
-const RemovingPanel = ({ channel, closeModal }) => {
+const RemovingPanel = ({ channels, channelId, closeModal }) => {
   const { t } = useTranslation();
   const socket = useContext(SocketContext);
   const dispatch = useDispatch();
-  const action = submitActionsMap.removing;
+  const action = submitActionsMap.remove;
+  const currentChannel = channels.find(({ id }) => id === channelId);
 
-  const removeChannel = (channelData) => () => {
+  const removeChannel = (channel) => () => {
     dispatch(setLoadingState({ loadingState: loadingStatesMap.loading }));
     try {
-      socket.emit(action, channelData, (response) => {
+      socket.emit(action, channel, (response) => {
         console.log(`${action} status: ${response.status}`);
       });
       dispatch(setLoadingState({ loadingState: loadingStatesMap.success }));
@@ -44,7 +49,7 @@ const RemovingPanel = ({ channel, closeModal }) => {
         <Button onClick={closeModal} variant="secondary" className="mr-2">
           {t('cancel')}
         </Button>
-        <Button onClick={removeChannel(channel)} variant="danger">
+        <Button onClick={removeChannel(currentChannel)} variant="danger">
           {t('remove')}
         </Button>
       </div>
@@ -52,52 +57,29 @@ const RemovingPanel = ({ channel, closeModal }) => {
   );
 };
 
-const buildSubmitHandler = (type, socket, closeModal, dispatch) => (
-  channel,
-  actions
-) => {
-  dispatch(setLoadingState({ loadingState: loadingStatesMap.loading }));
-  const { setSubmitting, resetForm } = actions;
-  setSubmitting(false);
-  const action = submitActionsMap[type];
-  try {
-    socket.emit(action, channel, (response) => {
-      console.log(`${action} status: ${response.status}`);
-    });
-    dispatch(setLoadingState({ loadingState: loadingStatesMap.success }));
-    resetForm();
-    closeModal();
-  } catch (error) {
-    console.log(error);
-    dispatch(setLoadingState({ loadingState: loadingStatesMap.failure }));
-  }
-};
+// *******************************************************************************************
 
-const SubmitPanel = ({ type, closeModal }) => {
+const PanelForm = ({
+  initialName,
+  validationSchema,
+  handleSubmit,
+  closeModal,
+}) => {
+  const textInput = useRef(null);
   const { t } = useTranslation();
-  const channels = useSelector((state) => state.channels);
-  const channelsNames = channels.map(({ name }) => name);
-
   const loadingState = useSelector((state) => state.loading);
   const isDisabled = loadingState === loadingStatesMap.loading;
 
-  const dispatch = useDispatch();
-  const socket = useContext(SocketContext);
-  const handleSubmit = buildSubmitHandler(type, socket, closeModal, dispatch);
-
-  const textInput = useRef(null);
   useEffect(() => {
     textInput.current.focus();
-  }, [textInput]);
+  });
 
   return (
     <Formik
       initialValues={{
-        name: '',
+        name: initialName,
       }}
-      validationSchema={yup.object().shape({
-        name: yup.string().required().notOneOf(channelsNames),
-      })}
+      validationSchema={validationSchema}
       validateOnBlur={false}
       validateOnChange={false}
       onSubmit={handleSubmit}
@@ -112,6 +94,7 @@ const SubmitPanel = ({ type, closeModal }) => {
             <FormGroup>
               <Field
                 innerRef={textInput}
+                onFocus={(e) => e.currentTarget.select()}
                 type="text"
                 name="name"
                 aria-label="add channel"
@@ -142,34 +125,113 @@ const SubmitPanel = ({ type, closeModal }) => {
   );
 };
 
-const ModalContent = ({ type, channelId, closeModal }) => {
-  const channels = useSelector((state) => state.channels);
-  const channel = channels.find(({ id }) => id === channelId);
+// const RemovingPanel = ({ closeModal }) => {
 
-  const modalTitleKeysMap = {
-    adding: 'addChannel',
-    renaming: 'renameChannel',
-    removing: 'removeChannel',
+//   // return jsx
+// };
+
+// { initialName, validationSchema, handleSubmit, closeModal }
+const RenamingPanel = ({ channels, channelId, closeModal }) => {
+  const socket = useContext(SocketContext);
+  const dispatch = useDispatch();
+  const channel = channels.find(({ id }) => id === channelId);
+  const initialName = channel.name;
+  const validationSchema = yup.object().shape({
+    name: yup.string().required(),
+  });
+
+  const renameChannel = (value, { setSubmitting }) => {
+    setSubmitting(false);
+    const action = submitActionsMap.rename;
+    try {
+      socket.emit(action, channel, (response) => {
+        console.log(`${action} status: ${response.status}`);
+      });
+      dispatch(setLoadingState({ loadingState: loadingStatesMap.success }));
+      closeModal();
+    } catch (error) {
+      console.log(error);
+      dispatch(setLoadingState({ loadingState: loadingStatesMap.failure }));
+    }
   };
 
-  const { t } = useTranslation();
   return (
-    <>
-      <Modal.Header closeButton>
-        <Modal.Title>{t(modalTitleKeysMap[type])}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {type === 'removing' ? (
-          <RemovingPanel channel={channel} closeModal={closeModal} />
-        ) : (
-          <SubmitPanel type={type} channel={channel} closeModal={closeModal} />
-        )}
-      </Modal.Body>
-    </>
+    <PanelForm
+      initialName={initialName}
+      validationSchema={validationSchema}
+      handleSubmit={renameChannel}
+      closeModal={closeModal}
+    />
   );
 };
 
+const AddingPanel = ({ channels, closeModal }) => {
+  const socket = useContext(SocketContext);
+  const dispatch = useDispatch();
+  const initialName = '';
+  const channelsNames = channels.map(({ name }) => name);
+  const validationSchema = yup.object().shape({
+    name: yup.string().required().notOneOf(channelsNames),
+  });
+
+  const addChannel = ({ name }, { setSubmitting }) => {
+    setSubmitting(false);
+    const action = submitActionsMap.add;
+    const channel = { name };
+    try {
+      socket.emit(action, channel, (response) => {
+        console.log(`${action} status: ${response.status}`);
+      });
+      dispatch(setLoadingState({ loadingState: loadingStatesMap.success }));
+      closeModal();
+    } catch (error) {
+      console.log(error);
+      dispatch(setLoadingState({ loadingState: loadingStatesMap.failure }));
+    }
+  };
+
+  return (
+    <PanelForm
+      initialName={initialName}
+      validationSchema={validationSchema}
+      handleSubmit={addChannel}
+      closeModal={closeModal}
+    />
+  );
+};
+
+const ControllPanel = ({ type, channelId, closeModal }) => {
+  const channels = useSelector((state) => state.channels);
+
+  console.log();
+
+  switch (type) {
+    case modalTypesMap.adding:
+      return <AddingPanel channels={channels} closeModal={closeModal} />;
+    case modalTypesMap.removing:
+      return (
+        <RemovingPanel
+          channels={channels}
+          channelId={channelId}
+          closeModal={closeModal}
+        />
+      );
+    case modalTypesMap.renaming:
+      return (
+        <RenamingPanel
+          channels={channels}
+          channelId={channelId}
+          closeModal={closeModal}
+        />
+      );
+    default:
+      console.log('BOOM !!!!');
+      throw new Error(`Unknown controll panel type: ${type}`);
+  }
+};
+
 const ModalWindow = () => {
+  const { t } = useTranslation();
   const { isVisible, type, channelId } = useSelector((state) => state.modal);
   const dispatch = useDispatch();
 
@@ -178,10 +240,27 @@ const ModalWindow = () => {
     dispatch(toggleModal({ modalConfig }));
   };
 
+  const modalTitleKeysMap = {
+    adding: 'addChannel',
+    renaming: 'renameChannel',
+    removing: 'removeChannel',
+  };
+
   return (
-    <Modal show={isVisible} onHide={closeModal}>
-      <ModalContent type={type} channelId={channelId} closeModal={closeModal} />
-    </Modal>
+    isVisible && (
+      <Modal show={isVisible} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>{t(modalTitleKeysMap[type])}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ControllPanel
+            type={type}
+            channelId={channelId}
+            closeModal={closeModal}
+          />
+        </Modal.Body>
+      </Modal>
+    )
   );
 };
 
