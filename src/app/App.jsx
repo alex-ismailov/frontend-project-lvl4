1cd545dabcc35c5757a2eda62c6e373c41d59c78
smlnx <alexander.is.dev@gmail.com>
2021-04-19 13:23:21 +0300

    Rename(src/app/App.jsx) RegistrationForm to SignupPage

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,13 +6,13 @@ import Chat from '../features/chat/Chat.jsx';
 import LoginForm from '../features/loginForm/LoginForm.jsx';
 import NoMatch from '../features/noMatch/NoMatch.jsx';
 import { UserNameProvider } from '../context/UserNameContext.js';
-import RegistrationPage from '../features/registrationForm/RegistrationForm.jsx';
+import SignupPage from '../features/signupPage/SignupPage.jsx';
 
 const App = () => (
   <div className="d-flex flex-column h-100">
     <Switch>
       <Route path="/login" render={() => <LoginForm />} />
-      <Route path="/signup" render={() => <RegistrationPage />} />
+      <Route path="/signup" render={() => <SignupPage />} />
       <Route
         exact
         path="/"
