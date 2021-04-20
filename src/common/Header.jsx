d3663dce7b66b43eec5b0c1206accc1f636d92a6
smlnx <alexander.is.dev@gmail.com>
2021-04-20 19:34:05 +0300

    Refactor(src/common/Header.jsx) use Navbar.Brand instead of <a>, use redirectToHome onClick handler instead of href attribute

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
 
 const Header = ({ render }) => {
   const { t } = useTranslation();
+  const history = useHistory();
+
+  const redirectToHome = (e) => {
+    e.preventDefault();
+    history.push('/');
+  };
 
   return (
     <Navbar className="mb-3" expand="lg" bg="light">
-      <a className="mr-auto navbar-brand" href="/">
-        {t('hexletChat')}
-      </a>
+      <Navbar.Brand onClick={redirectToHome}>{t('hexletChat')}</Navbar.Brand>
       {render && render()}
     </Navbar>
   );
