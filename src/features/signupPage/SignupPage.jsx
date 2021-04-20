85a3dffa534f076c6dcbe63a4ee547da305c8f82
smlnx <alexander.is.dev@gmail.com>
2021-04-20 19:13:27 +0300

    Add(src/features/signupPage/SignupPage.jsx) validation styling

--- src/features/signupPage/SignupPage.jsx
+++ src/features/signupPage/SignupPage.jsx
@@ -20,21 +20,23 @@ import routes from '../../common/routes.js';
 const SignupForm = () => {
   const { t } = useTranslation();
   const history = useHistory();
-
-  const validationSchema = yup.object().shape({
-    username: yup.string().min(3).max(20).required(),
-    password: yup.string().min(6).required(),
-    confirmPassword: yup.string().oneOf([yup.ref('password'), null]),
-  });
-
   const usernameInput = useRef(null);
+
   useEffect(() => {
     usernameInput.current.focus();
   });
 
+  const validationSchema = yup.object().shape({
+    username: yup.string().required().min(3).max(20),
+    password: yup.string().required().min(6),
+    confirmPassword: yup
+      .string()
+      .required()
+      .oneOf([yup.ref('password'), null]),
+  });
+
   const registerNewUser = async ({ username, password }, actions) => {
     actions.setSubmitting(false);
-    console.log(actions);
     const credentials = { username, password };
     try {
       const response = await axios.post(routes.signupPath(), credentials);
@@ -43,17 +45,12 @@ const SignupForm = () => {
       localStorage.setItem('username', data.username);
       history.push('/');
     } catch (e) {
-      console.log(e);
-      if (!e.response) {
-        actions.setStatus('failure');
-        actions.setFieldTouched('confirmPassword', true);
-        actions.setFieldError('confirmPassword', 'networkError');
+      if (e.message === 'Network Error') {
+        actions.setStatus({ isValid: false, message: 'networkError' });
         return;
       }
-      actions.setStatus('failure');
-      actions.setFieldTouched('confirmPassword', true);
-      actions.setFieldError('confirmPassword', 'userAlreadyExists');
-      //
+      usernameInput.current.focus();
+      actions.setStatus({ isValid: false, message: 'userAlreadyExists' });
     }
   };
 
@@ -65,80 +62,85 @@ const SignupForm = () => {
         confirmPassword: '',
       }}
       validationSchema={validationSchema}
-      // validateOnBlur={false}
       onSubmit={registerNewUser}
     >
-      {(props) => {
-        const { isValid, errors, touched, status } = props;
-        return (
-          <Form className="p-3">
-            <FormGroup>
-              <FormLabel htmlFor="username">{t('username')}</FormLabel>
-              <Field
-                innerRef={usernameInput}
-                onFocus={(e) => e.currentTarget.select()}
-                // autoFocus
-                type="text"
-                name="username"
-                placeholder={t('minMaxSymbols')}
-                autoComplete="username"
-                id="username"
-                className={cn('form-control', {
-                  'is-invalid': (errors.username && touched.username) || status,
-                })}
-              />
-              <ErrorMessage
-                name="username"
-                render={() => <Feedback message="minMaxSymbols" />}
-              />
-            </FormGroup>
-            <FormGroup>
-              <FormLabel htmlFor="password">{t('password')}</FormLabel>
-              <Field
-                type="password"
-                name="password"
-                placeholder={t('minChars')}
-                autoComplete="new-password"
-                id="password"
-                className={cn('form-control', {
-                  'is-invalid': (errors.password && touched.password) || status,
-                })}
-              />
-              <ErrorMessage
-                name="password"
-                render={(message) => <Feedback message={message} />}
-              />
-            </FormGroup>
-            <FormGroup>
-              <FormLabel htmlFor="confirmPassword">
-                {t('confirmPassword')}
-              </FormLabel>
-              <Field
-                type="password"
-                name="confirmPassword"
-                placeholder={t('passwordsMustMatch')}
-                autoComplete="new-password"
-                id="confirmPassword"
-                className={cn('form-control', {
-                  'is-invalid': (errors && touched.confirmPassword) || status,
-                })}
-              />
-              <ErrorMessage
-                name="confirmPassword"
-                render={(message) => <Feedback message={message} />}
-              />
-            </FormGroup>
-            <Button
-              type="submit"
-              disabled={!isValid}
-              variant="outline-primary"
-              className="w-100"
-            >
-              {t('signup')}
-            </Button>
-          </Form>
-        );
-      }}
+      {({ isValid, errors, touched, status }) => (
+        <Form className="p-3">
+          <FormGroup>
+            <FormLabel htmlFor="username">{t('username')}</FormLabel>
+            <Field
+              innerRef={usernameInput}
+              onFocus={(e) => e.currentTarget.select()}
+              type="text"
+              name="username"
+              placeholder={t('minMaxSymbols')}
+              autoComplete="username"
+              id="username"
+              className={cn('form-control', {
+                'is-invalid':
+                  (errors.username && touched.username) ||
+                  (status && !status.isValid),
+              })}
+            />
+            <ErrorMessage
+              name="username"
+              render={(msg) => {
+                const message = msg === 'required' ? msg : 'minMaxSymbols';
+                return <Feedback message={message} />;
+              }}
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel htmlFor="password">{t('password')}</FormLabel>
+            <Field
+              type="password"
+              name="password"
+              placeholder={t('minChars')}
+              autoComplete="new-password"
+              id="password"
+              className={cn('form-control', {
+                'is-invalid':
+                  (errors.password && touched.password) ||
+                  (status && !status.isValid),
+              })}
+            />
+            <ErrorMessage
+              name="password"
+              render={(message) => <Feedback message={message} />}
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel htmlFor="confirmPassword">
+              {t('confirmPassword')}
+            </FormLabel>
+            <Field
+              type="password"
+              name="confirmPassword"
+              placeholder={t('passwordsMustMatch')}
+              autoComplete="new-password"
+              id="confirmPassword"
+              className={cn('form-control', {
+                'is-invalid':
+                  (errors.confirmPassword && touched.confirmPassword) ||
+                  (status && !status.isValid),
+              })}
+            />
+            <ErrorMessage
+              name="confirmPassword"
+              render={(message) => <Feedback message={message} />}
+            />
+            {status && !status.isValid && <Feedback message={status.message} />}
+          </FormGroup>
+          <Button
+            type="submit"
+            disabled={!isValid}
+            variant="outline-primary"
+            className="w-100"
+          >
+            {t('signup')}
+          </Button>
+        </Form>
+      )}
     </Formik>
   );
 };
