f6a0f8b556024f6df9382ca79d4d10014f9ec0c2
smlnx <alexander.is.dev@gmail.com>
2021-04-21 16:14:27 +0300

    Add( __tests__/) frontend.test.js stub test with my legacy tests draft

---
+++ __tests__/frontend.test.js
@@ -0,0 +1,34 @@
+// // @ts-check // Ругается на test
+
+// import React from 'react';
+
+// import init from '../src/init.jsx';
+// import App from '../src/App.jsx';
+
+// import testingLibraryReact from '@testing-library/react';
+// const { render, screen } = testingLibraryReact;
+
+// import { render, screen } from '@testing-library/react';
+
+// STUB
+test('stub test', () => {});
+
+// const gon = {
+//   channels: [
+//     { id: 1, name: 'general', removable: false },
+//     { id: 2, name: 'random', removable: false },
+//   ],
+//   messages: [],
+//   currentChannelId: 1,
+// };
+
+// let App;
+// beforeEach(() => {
+//   App = init(gon);
+// });
+
+// test('renders App component', () => {
+//   render(<App />);
+
+//   screen.debug();
+// });
