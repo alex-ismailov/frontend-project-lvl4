24058f9bfe3fed2c3b96a337d156db52f8af68dd
smlnx <alexander.is.dev@gmail.com>
2021-04-26 18:01:38 +0300

    Remove(src/) rollbar.js. Move Rollbar configuration to src/index.js

--- src/rollbar.js
+++
@@ -1,9 +0,0 @@
-import Rollbar from 'rollbar';
-
-export default () =>
-  new Rollbar({
-    accessToken: process.env.ROLLBAR_TOKEN,
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-    enabled: process.env.NODE_ENV === 'production',
-  });
