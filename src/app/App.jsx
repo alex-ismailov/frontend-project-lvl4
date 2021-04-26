748e64899b743c21530d2cd9394657c1aaa8c245
smlnx <alexander.is.dev@gmail.com>
2021-04-26 08:29:17 +0300

    Refactor(src/app/App.jsx) authorization flow. Add AuthProvider and ChatRoute

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,35 +1,67 @@
 // @ts-check
 
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import _ from 'lodash';
 import Chat from '../features/chat/Chat.jsx';
 import LoginForm from '../features/loginForm/LoginForm.jsx';
 import NoMatch from '../features/noMatch/NoMatch.jsx';
-import { UserNameProvider } from '../context/UserNameContext.js';
+import authContext from '../context/authContext.jsx';
+import useAuth from '../hooks/useAuth.jsx';
 import SignupPage from '../features/signupPage/SignupPage.jsx';
+
+const AuthProvider = ({ children }) => {
+  const isLoggedIn = () => _.has(localStorage, 'token');
+
+  const logIn = (token, username) => {
+    localStorage.setItem('token', token);
+    localStorage.setItem('username', username);
+  };
+
+  const logOut = () => {
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');
+  };
+
+  return (
+    <authContext.Provider value={{ isLoggedIn, logIn, logOut }}>
+      {children}
+    </authContext.Provider>
+  );
+};
+
+const ChatRoute = ({ children, path }) => {
+  const auth = useAuth();
+
+  return (
+    <Route
+      path={path}
+      render={({ location }) =>
+        // @ts-ignore
+        auth.isLoggedIn() ? (
+          children
+        ) : (
+          <Redirect to={{ pathname: '/login', state: { from: location } }} />
+        )
+      }
+    />
+  );
+};
 
 const App = () => (
   <div className="d-flex flex-column h-100">
-    <Switch>
-      <Route path="/login" render={() => <LoginForm />} />
-      <Route path="/signup" render={() => <SignupPage />} />
-      <Route
-        exact
-        path="/"
-        render={() => {
-          if (localStorage.token) {
-            const { username } = localStorage;
-            return (
-              <UserNameProvider value={username}>
-                <Chat />
-              </UserNameProvider>
-            );
-          }
-          return <Redirect push to="/login" />;
-        }}
-      />
-      <Route path="*" render={() => <NoMatch />} />
-    </Switch>
+    <AuthProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" render={() => <LoginForm />} />
+          <Route path="/signup" render={() => <SignupPage />} />
+          <ChatRoute path="/">
+            <Chat />
+          </ChatRoute>
+          <Route path="*" render={() => <NoMatch />} />
+        </Switch>
+      </BrowserRouter>
+    </AuthProvider>
   </div>
 );
 
