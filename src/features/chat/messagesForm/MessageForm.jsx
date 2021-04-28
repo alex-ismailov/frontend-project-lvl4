7d4ab2349c0f36bf96d0258c30a55593957f619c
smlnx <alexander.is.dev@gmail.com>
2021-04-28 22:30:38 +0300

    Refactor(src/features/chat/messagesForm/MessageForm.jsx) remove depedency on loadingState

--- src/features/chat/messagesForm/MessageForm.jsx
+++ src/features/chat/messagesForm/MessageForm.jsx
@@ -1,43 +1,19 @@
 // @ts-check
 
 import React, { useContext, useRef, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import { Form, Button, InputGroup } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import SocketContext from '../../../context/SocketContext.js';
-import {
-  loadingStatesMap,
-  setLoadingState,
-} from '../../../app/loadingSlice.js';
-
-// const sendMessage = async (message, socket, dispatch) => {
-//   dispatch(setLoadingState({ loadingState: loadingStatesMap.loading }));
-//   try {
-//     socket.emit('newMessage', message, (response) => {
-//       console.log(`Message sending status: ${response.status}`);
-//     });
-//     dispatch(setLoadingState({ loadingState: loadingStatesMap.success }));
-//   } catch (error) {
-//     console.log(error);
-//     // setTimeout(() => {
-//     //   sendMessage(message, socket);
-//     // }, 3000); // Это пока под вопросом
-//     dispatch(setLoadingState({ loadingState: loadingStatesMap.failure }));
-//   }
-// };
 
 const MessageForm = () => {
   const { t } = useTranslation();
-  const dispatch = useDispatch();
   const socket = useContext(SocketContext);
   const username = localStorage.getItem('username');
   // @ts-ignore
   const currentChannelId = useSelector((state) => state.currentChannelId);
-  // @ts-ignore
-  const loadingState = useSelector((state) => state.loading);
-  const isDisabled = loadingState === loadingStatesMap.loading;
   const inputRef = useRef(null);
 
   useEffect(() => {
@@ -52,19 +28,8 @@ const MessageForm = () => {
       body: yup.string().required(),
     }),
     validateOnBlur: false,
-    // onSubmit: async ({ body }, { setSubmitting, resetForm }) => {
-    //   setSubmitting(false);
-    //   const message = {
-    //     nickname: username,
-    //     body,
-    //     channelId: currentChannelId,
-    //   };
-    //   await sendMessage(message, socket, dispatch);
-    //   resetForm();
-    // },
     onSubmit: ({ body }, { setSubmitting, resetForm }) => {
       setSubmitting(false);
-      dispatch(setLoadingState({ loadingState: loadingStatesMap.loading }));
       const message = {
         nickname: username,
         body,
@@ -74,11 +39,9 @@ const MessageForm = () => {
         socket.emit('newMessage', message, (response) => {
           console.log(`Message sending status: ${response.status}`);
         });
-        dispatch(setLoadingState({ loadingState: loadingStatesMap.success }));
         resetForm();
       } catch (error) {
         console.log(error);
-        dispatch(setLoadingState({ loadingState: loadingStatesMap.failure }));
       }
     },
   });
@@ -103,7 +66,7 @@ const MessageForm = () => {
             ref={inputRef}
           />
           <InputGroup.Append>
-            <Button type="submit" variant="primary" disabled={isDisabled}>
+            <Button type="submit" variant="primary">
               {t('send')}
             </Button>
           </InputGroup.Append>
