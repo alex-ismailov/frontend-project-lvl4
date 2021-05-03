93c7d1328e90028ea8211de4e9e9688419024f41
alex-ismailov <alexx.ismailov@gmail.com>
2021-05-03 23:07:19 +0300

    Add(src/features/messages/messagesSlice.js) extraReducer on initChannels action

--- src/features/messages/messagesSlice.js
+++ src/features/messages/messagesSlice.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 import { createSlice } from '@reduxjs/toolkit';
-import { removeChannel } from '../channelsInfo/channelsSlice.js';
+import { initChannels, removeChannel } from '../channelsInfo/channelsSlice.js';
 
 const slice = createSlice({
   name: 'messages',
@@ -10,16 +10,20 @@ const slice = createSlice({
     addNewMessage: (state, { payload: { message } }) => {
       state.push(message);
     },
-    initMessages: (state, { payload: { messages } }) => messages,
   },
   extraReducers: (builder) => {
-    builder.addCase(removeChannel, (state, action) => {
-      const { id } = action.payload;
-      return state.filter((message) => message.channelId !== id);
-    });
+    builder
+      .addCase(removeChannel, (state, action) => {
+        const { id } = action.payload;
+        return state.filter((message) => message.channelId !== id);
+      })
+      .addCase(initChannels, (state, action) => {
+        const { messages } = action.payload.data;
+        state.push(...messages);
+      });
   },
 });
 
-export const { addNewMessage, initMessages } = slice.actions;
+export const { addNewMessage } = slice.actions;
 
 export default slice.reducer;
