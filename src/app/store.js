256b1f9311823f91a47115b1098c994f7dcb778b
alex-ismailov <alexx.ismailov@gmail.com>
2021-05-03 11:11:03 +0300

    Remove(src/app/store.js) currentChannelIdReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from '../features/chat/channels/channelsSlice.js';
 import messagesReducer from '../features/chat/messages/messagesSlice.js';
-import currentChannelIdReducer from '../features/chat/channels/currentChannelIdSlice.js';
 import modalReducer from '../features/modal/ModalWindowSlice.js';
 
 export default () => {
   const store = configureStore({
     reducer: {
-      channels: channelsReducer,
+      channelsInfo: channelsReducer,
       messages: messagesReducer,
-      currentChannelId: currentChannelIdReducer,
       modal: modalReducer,
     },
   });
