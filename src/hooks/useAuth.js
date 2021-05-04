a6d1f73ec0f1739f1481cb72a83527b790bbbb8f
alex-ismailov <alexx.ismailov@gmail.com>
2021-05-04 20:58:55 +0300

    Refactor(src/hooks/) change useAuth hook format from .jsx to .js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,7 +2,7 @@
 
 import { useContext } from 'react';
 
-import authContext from '../context/authContext.jsx';
+import authContext from '../context/authContext.js';
 
 const useAuth = () => useContext(authContext);
 
