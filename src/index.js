49c99baf46e5da8788d902825baeda428f36b0e4
alex-ismailov <alexx.ismailov@gmail.com>
2021-05-09 12:11:42 +0300

    Remove(src/index.js) logs

--- src/index.js
+++ src/index.js
@@ -12,16 +12,12 @@ const runApp = async () => {
   const mode = process.env.NODE_ENV;
 
   // eslint-disable-next-line
-  const rollbar = new Rollbar({
+  new Rollbar({
     accessToken: process.env.ROLLBAR_TOKEN,
     captureUncaught: true,
     captureUnhandledRejections: true,
     enabled: mode === 'production',
   });
-  // log a generic message and send to rollbar
-  rollbar.log('Test rollbar from heroku');
-  console.log(`mode: ${mode}`);
-  console.log(`process.env.ROLLBAR_TOKEN: ${process.env.ROLLBAR_TOKEN}`);
 
   if (mode !== 'production') {
     localStorage.debug = 'chat:*';
