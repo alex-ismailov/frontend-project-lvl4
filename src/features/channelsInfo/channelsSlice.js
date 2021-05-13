5d9fddeccdfa185dbf6235233ee2c760c73f9014
alex-ismailov <alexx.ismailov@gmail.com>
2021-05-13 23:36:15 +0300

    Refactor(src/features/channelsInfo/channelsSlice.js) there is no need to return the state

--- src/features/channelsInfo/channelsSlice.js
+++ src/features/channelsInfo/channelsSlice.js
@@ -15,28 +15,23 @@ const slice = createSlice({
     addChannel: (state, { payload: { channel } }) => {
       state.channels.push(channel);
       state.currentChannelId = channel.id;
-      return state;
     },
     removeChannel: (state, { payload: { id } }) => {
       state.channels = state.channels.filter((channel) => channel.id !== id);
       state.currentChannelId = GENERAL_ID;
-      return state;
     },
     renameChannel: (state, { payload: { channel: changedСhannel } }) => {
       const { id, name } = changedСhannel;
       state.channels = state.channels
         .map((channel) => (channel.id === id ? { ...channel, name } : channel));
-      return state;
     },
     initChannels: (state, { payload: { data } }) => {
       const { channels, currentChannelId } = data;
       state.channels = channels;
       state.currentChannelId = currentChannelId;
-      return state;
     },
     setCurrentChannelId: (state, { payload: { channelId } }) => {
       state.currentChannelId = channelId;
-      return state;
     },
   },
 });
